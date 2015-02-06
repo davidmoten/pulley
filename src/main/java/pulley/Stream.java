983e636a7e04e99bd55c4a611221c67dbf4d52cc
Dave Moten <davidmoten@gmail.com>
2015-02-06 17:07:30 +1100

    dev

--- src/main/java/pulley/Stream.java
+++ src/main/java/pulley/Stream.java
@@ -1,5 +1,13 @@
 package pulley;
 
-public interface Stream<T> extends Promise<Cons<T>> {
+public class Stream<T> {
+	private final Promise<Cons<T>> promise;
 
+	public Stream(Promise<Cons<T>> promise) {
+		this.promise = promise;
+	}
+
+	public <R> Stream<R> map(F1<? super T, ? extends R> f) {
+
+	}
 }
