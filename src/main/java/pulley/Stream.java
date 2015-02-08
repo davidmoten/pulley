81ad27ad972a291c1d69f55843495201a36d7f2e
Dave Moten <davidmoten@gmail.com>
2015-02-08 18:23:23 +1100

    dev

--- src/main/java/pulley/Stream.java
+++ src/main/java/pulley/Stream.java
@@ -9,8 +9,22 @@ public class Stream<T> {
 		this.promise = promise;
 	}
 
+	public static <T> Stream<T> create(Promise<Optional<Cons<T>>> promise) {
+		return new Stream<T>(promise);
+	}
+
 	public <R> Stream<R> map(F1<? super T, ? extends R> f) {
 		return new Stream<R>(promise.map(F.optional(F.cons(f))));
+	}
+
+	public static <T> Stream<T> just(T t) {
+		return create(new CompletedPromise<Optional<Cons<T>>>(
+				Optional.of(new Cons<T>(t, Stream.<T> empty()))));
+	}
+
+	public static <T> Stream<T> empty() {
+		return create(new CompletedPromise<Optional<Cons<T>>>(
+				Optional.<Cons<T>> absent()));
 	}
 
 	public void forEach(A1<T> action) {
