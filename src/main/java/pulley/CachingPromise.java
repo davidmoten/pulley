3193cfaab9a13d1b6905add60cdc39644d06df6a
Dave Moten <davidmoten@gmail.com>
2015-02-10 21:36:55 +1100

    dev

---
+++ src/main/java/pulley/CachingPromise.java
@@ -0,0 +1,32 @@
+package pulley;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CachingPromise<T> implements Promise<T> {
+
+    private final Promise<T> promise;
+    private final AtomicBoolean calculated = new AtomicBoolean(false);
+    private volatile T value;
+
+    public CachingPromise(Promise<T> promise) {
+        this.promise = promise;
+    }
+
+    @Override
+    public T get() {
+        if (calculated.compareAndSet(false, true))
+            value = promise.get();
+        return value;
+    }
+
+    @Override
+    public A0 closeAction() {
+        return promise.closeAction();
+    }
+
+    @Override
+    public Scheduler scheduler() {
+        return promise.scheduler();
+    }
+
+}
--- src/main/java/pulley/streams/Merge.java
+++ src/main/java/pulley/streams/Merge.java
@@ -2,6 +2,7 @@ package pulley.streams;
 
 import static pulley.Stream.stream;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -16,76 +17,72 @@ import pulley.Stream;
 import pulley.util.Optional;
 
 public class Merge {
-	public static <T> Stream<T> create(Stream<T> s1, Stream<T> s2) {
-		return stream(factory(s1, s2));
-	}
+    public static <T> Stream<T> create(Stream<T> s1, Stream<T> s2) {
+        return stream(factory(s1, s2));
+    }
 
-	private static <T> Factory<Promise<Optional<Cons<T>>>> factory(
-			final Stream<T> s1, final Stream<T> s2) {
-		return new Factory<Promise<Optional<Cons<T>>>>() {
-			@Override
-			public Promise<Optional<Cons<T>>> create() {
-				Promise<Optional<Cons<T>>> p1 = s1.factory().create();
-				Promise<Optional<Cons<T>>> p2 = s2.factory().create();
-				return new MergePromise<T>(p1, p2);
-			}
-		};
-	}
+    private static <T> Factory<Promise<Optional<Cons<T>>>> factory(final Stream<T> s1,
+            final Stream<T> s2) {
+        return new Factory<Promise<Optional<Cons<T>>>>() {
+            @Override
+            public Promise<Optional<Cons<T>>> create() {
+                Promise<Optional<Cons<T>>> p1 = s1.factory().create();
+                Promise<Optional<Cons<T>>> p2 = s2.factory().create();
+                return new MergePromise<T>(p1, p2);
+            }
+        };
+    }
 
-	private static class MergePromise<T> implements Promise<Optional<Cons<T>>> {
+    private static class MergePromise<T> implements Promise<Optional<Cons<T>>> {
 
-		private Promise<Optional<Cons<T>>> p1;
-		private Promise<Optional<Cons<T>>> p2;
+        private final List<Promise<Optional<Cons<T>>>> promises;
 
-		public MergePromise(Promise<Optional<Cons<T>>> p1,
-				Promise<Optional<Cons<T>>> p2) {
-			this.p1 = p1;
-			this.p2 = p2;
-		}
+        MergePromise(List<Promise<Optional<Cons<T>>>> promises) {
+            this.promises = promises;
+        }
 
-		@Override
-		public Optional<Cons<T>> get() {
-			// blocking !!!!
-			final AtomicReference<Byte> which = new AtomicReference<Byte>(
-					(byte) 0);
-			final AtomicReference<Optional<Cons<T>>> ref = new AtomicReference<Optional<Cons<T>>>();
-			final CountDownLatch latch = new CountDownLatch(1);
-			p1.scheduler().schedule(new A0() {
-				@Override
-				public void call() {
-					Optional<Cons<T>> value = p1.get();
-					if (which.compareAndSet((byte) 0, (byte) 1)) {
-						ref.set(value);
-						latch.countDown();
-					}
-				}
-			});
-			p2.scheduler().schedule(new A0() {
-				@Override
-				public void call() {
-					Optional<Cons<T>> value = p2.get();
-					if (which.compareAndSet((byte) 0, (byte) 2)) {
-						ref.set(value);
-						latch.countDown();
-					}
-				}
-			});
-			try {
-				latch.await();
-			} catch (InterruptedException e) {
-				throw new RuntimeException(e);
-			}
-		}
+        @Override
+        public Optional<Cons<T>> get() {
+            // blocking !!!!
+            final AtomicReference<Byte> which = new AtomicReference<Byte>((byte) 0);
+            final AtomicReference<Optional<Cons<T>>> ref = new AtomicReference<Optional<Cons<T>>>();
+            final CountDownLatch latch = new CountDownLatch(1);
+            p1.scheduler().schedule(new A0() {
+                @Override
+                public void call() {
+                    Optional<Cons<T>> value = p1.get();
+                    if (which.compareAndSet((byte) 0, (byte) 1)) {
+                        ref.set(value);
+                        latch.countDown();
+                    }
+                }
+            });
+            p2.scheduler().schedule(new A0() {
+                @Override
+                public void call() {
+                    Optional<Cons<T>> value = p2.get();
+                    if (which.compareAndSet((byte) 0, (byte) 2)) {
+                        ref.set(value);
+                        latch.countDown();
+                    }
+                }
+            });
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
-		@Override
-		public A0 closeAction() {
-			return Actions.sequence(p1.closeAction(), p2.closeAction());
-		}
+        @Override
+        public A0 closeAction() {
+            return Actions.sequence(p1.closeAction(), p2.closeAction());
+        }
 
-		@Override
-		public Scheduler scheduler() {
-			return Schedulers.immediate();
-		}
+        @Override
+        public Scheduler scheduler() {
+            return Schedulers.immediate();
+        }
 
-	}
+    }
 }
