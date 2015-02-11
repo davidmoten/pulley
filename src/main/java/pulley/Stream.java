c02ca4aa3e214591cb1450eeb5986eba148b50e0
Dave Moten <davidmoten@gmail.com>
2015-02-11 21:26:31 +1100

    add toList

--- src/main/java/pulley/Stream.java
+++ src/main/java/pulley/Stream.java
@@ -7,123 +7,123 @@ import java.util.List;
 import pulley.util.Optional;
 
 public class Stream<T> {
-	private final Factory<Promise<Optional<Cons<T>>>> factory;
+    private final Factory<Promise<Optional<Cons<T>>>> factory;
 
-	public Stream(Factory<Promise<Optional<Cons<T>>>> factory) {
-		this.factory = factory;
-	}
+    public Stream(Factory<Promise<Optional<Cons<T>>>> factory) {
+        this.factory = factory;
+    }
 
-	public Factory<Promise<Optional<Cons<T>>>> factory() {
-		return factory;
-	}
+    public Factory<Promise<Optional<Cons<T>>>> factory() {
+        return factory;
+    }
 
-	public static <T> Stream<T> stream(
-			Factory<Promise<Optional<Cons<T>>>> factory) {
-		return new Stream<T>(factory);
-	}
+    public static <T> Stream<T> stream(Factory<Promise<Optional<Cons<T>>>> factory) {
+        return new Stream<T>(factory);
+    }
 
-	public <R> Stream<R> map(final F1<? super T, ? extends R> f) {
-		final F1<Optional<Cons<T>>, Optional<Cons<R>>> g = F
-				.optional(F.cons(f));
-		Factory<Promise<Optional<Cons<R>>>> factory2 = new Factory<Promise<Optional<Cons<R>>>>() {
-			@Override
-			public Promise<Optional<Cons<R>>> create() {
-				final Promise<Optional<Cons<T>>> p = factory.create();
-				return new Promise<Optional<Cons<R>>>() {
-					@Override
-					public Optional<Cons<R>> get() {
-						return g.call(p.get());
-					}
+    public <R> Stream<R> map(final F1<? super T, ? extends R> f) {
+        final F1<Optional<Cons<T>>, Optional<Cons<R>>> g = F.optional(F.cons(f));
+        Factory<Promise<Optional<Cons<R>>>> factory2 = new Factory<Promise<Optional<Cons<R>>>>() {
+            @Override
+            public Promise<Optional<Cons<R>>> create() {
+                final Promise<Optional<Cons<T>>> p = factory.create();
+                return new Promise<Optional<Cons<R>>>() {
+                    @Override
+                    public Optional<Cons<R>> get() {
+                        return g.call(p.get());
+                    }
 
-					@Override
-					public A0 closeAction() {
-						return p.closeAction();
-					}
+                    @Override
+                    public A0 closeAction() {
+                        return p.closeAction();
+                    }
 
-					@Override
-					public Scheduler scheduler() {
-						return Schedulers.immediate();
-					}
-				};
-			}
+                    @Override
+                    public Scheduler scheduler() {
+                        return Schedulers.immediate();
+                    }
+                };
+            }
 
-		};
-		return stream(factory2);
-	}
+        };
+        return stream(factory2);
+    }
 
-	public Stream<List<T>> toList() {
-		Factory<Promise<Optional<Cons<List<T>>>>> factory2 = new Factory<Promise<Optional<Cons<List<T>>>>>() {
-			@Override
-			public Promise<Optional<Cons<List<T>>>> create() {
-				final Promise<Optional<Cons<T>>> p = factory.create();
-				Promise<Optional<Cons<List<T>>>> p2 = new Promise<Optional<Cons<List<T>>>>() {
+    public Stream<List<T>> toList() {
+        Factory<Promise<Optional<Cons<List<T>>>>> factory2 = new Factory<Promise<Optional<Cons<List<T>>>>>() {
+            @Override
+            public Promise<Optional<Cons<List<T>>>> create() {
+                final Promise<Optional<Cons<T>>> p = factory.create();
+                return new Promise<Optional<Cons<List<T>>>>() {
 
-					@Override
-					public Optional<Cons<List<T>>> get() {
-						final List<T> list = Collections
-								.synchronizedList(new ArrayList<T>());
-						A1 addToList = new A1<T>() {
-							@Override
-							public void call(T t) {
-								list.add(t);
-							}
-						};
-						forEach(p, addToList);
-						return Optional.of(Cons.cons(list,
-								Promises.<Cons<List<T>>> empty()));
-					}
+                    @Override
+                    public Optional<Cons<List<T>>> get() {
+                        final List<T> list = Collections.synchronizedList(new ArrayList<T>());
+                        A1 addToList = new A1<T>() {
+                            @Override
+                            public void call(T t) {
+                                list.add(t);
+                            }
+                        };
+                        forEach(p, addToList);
+                        return Optional.of(Cons.cons(list, Promises.<Cons<List<T>>> empty()));
+                    }
 
-					@Override
-					public A0 closeAction() {
-						return p.closeAction();
-					}
+                    @Override
+                    public A0 closeAction() {
+                        return p.closeAction();
+                    }
 
-					@Override
-					public Scheduler scheduler() {
-						return p.scheduler();
-					}
-				};
-			}
-		};
-		return stream(factory2);
-	}
+                    @Override
+                    public Scheduler scheduler() {
+                        return p.scheduler();
+                    }
+                };
+            }
+        };
+        return stream(factory2);
+    }
 
-	public void forEach(A1<? super T> action) {
-		Promise<Optional<Cons<T>>> p = factory.create();
-		forEach(p, action);
-	}
+    public void forEach(A1<? super T> action) {
+        Promise<Optional<Cons<T>>> p = factory.create();
+        forEach(p, action);
+    }
 
-	private static <T> void forEach(Promise<Optional<Cons<T>>> p,
-			A1<? super T> action) {
-		while (true) {
-			Optional<Cons<T>> value = p.get();
-			if (value.isPresent()) {
-				action.call(value.get().head());
-				p = value.get().tail();
-			} else {
-				p.closeAction().call();
-				return;
-			}
-		}
-	}
+    private static <T> void forEach(Promise<Optional<Cons<T>>> p, final A1<? super T> action) {
+        while (true) {
+            final Optional<Cons<T>> value = p.get();
+            if (value.isPresent()) {
+                A0 a = new A0() {
+                    @Override
+                    public void call() {
+                        action.call(value.get().head());
+                    }
+                };
+                p.scheduler().schedule(a);
+                p = value.get().tail();
+            } else {
+                p.closeAction().call();
+                return;
+            }
+        }
+    }
 
-	public void forEach() {
-		forEach(Actions.doNothing1());
-	}
+    public void forEach() {
+        forEach(Actions.doNothing1());
+    }
 
-	public T single() {
-		Optional<Cons<T>> c = factory.create().get();
-		final T value;
-		if (c.isPresent())
-			value = c.get().head();
-		else
-			throw new RuntimeException("expected one item but no items emitted");
-		Optional<Cons<T>> c2 = c.get().tail().get();
-		if (c2.isPresent())
-			throw new RuntimeException(
-					"expected one item but more than one emitted");
-		else
-			return value;
-	}
+    public T single() {
+        Optional<Cons<T>> c = factory.create().get();
+        final T value;
+        if (c.isPresent())
+            value = c.get().head();
+        else
+            throw new RuntimeException("expected one item but no items emitted");
+        Optional<Cons<T>> c2 = c.get().tail().get();
+        if (c2.isPresent())
+            throw new RuntimeException("expected one item but more than one emitted");
+        else
+            return value;
+    }
 
 }
--- src/test/java/pulley/StreamsTest.java
+++ src/test/java/pulley/StreamsTest.java
@@ -4,83 +4,89 @@ import static org.junit.Assert.assertEquals;
 import static pulley.Actions.println;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 public class StreamsTest {
 
-	@Test
-	public void testHelloWorld() {
-		Streams.just("hello world").forEach(println());
-	}
+    @Test
+    public void testHelloWorld() {
+        Streams.just("hello world").forEach(println());
+    }
 
-	@Test
-	public void testMap() {
-		assertEquals(2, (int) Streams.just(1).map(plusOne()).single());
-	}
+    @Test
+    public void testMap() {
+        assertEquals(2, (int) Streams.just(1).map(plusOne()).single());
+    }
 
-	private static F1<Integer, Integer> plusOne() {
-		return new F1<Integer, Integer>() {
-			@Override
-			public Integer call(Integer t) {
-				return t + 1;
-			}
-		};
-	}
+    private static F1<Integer, Integer> plusOne() {
+        return new F1<Integer, Integer>() {
+            @Override
+            public Integer call(Integer t) {
+                return t + 1;
+            }
+        };
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void testSingleFailsWithTwoItems() {
-		Streams.just(1, 2).single();
-	}
+    @Test(expected = RuntimeException.class)
+    public void testSingleFailsWithTwoItems() {
+        Streams.just(1, 2).single();
+    }
 
-	@Test
-	public void testSingle() {
-		assertEquals(1, (int) Streams.just(1).single());
-	}
+    @Test
+    public void testSingle() {
+        assertEquals(1, (int) Streams.just(1).single());
+    }
 
-	@Test
-	public void testTwoPrinted() {
-		Streams.just(3, 4).forEach(println());
-	}
+    @Test
+    public void testTwoPrinted() {
+        Streams.just(3, 4).forEach(println());
+    }
 
-	@Test
-	public void testIterable() {
-		Streams.from(Arrays.asList(1, 2)).forEach(println());
-	}
+    @Test
+    public void testIterable() {
+        Streams.from(Arrays.asList(1, 2)).forEach(println());
+    }
 
-	@Test
-	public void testRange() {
-		Streams.range(1, 1000000).forEach();
-	}
+    @Test
+    public void testRange() {
+        Streams.range(1, 1000000).forEach();
+    }
 
-	@Test
-	public void testTrampoline() {
-		Scheduler s = Schedulers.trampoline();
-		s.schedule(Actions.info("hello"));
-		s.schedule(Actions.info("world"));
-		s.schedule(Actions.info("from trampoline"));
-	}
+    @Test
+    public void testTrampoline() {
+        Scheduler s = Schedulers.trampoline();
+        s.schedule(Actions.info("hello"));
+        s.schedule(Actions.info("world"));
+        s.schedule(Actions.info("from trampoline"));
+    }
 
-	@Test
-	public void testImmediate() {
-		Scheduler s = Schedulers.immediate();
-		s.schedule(Actions.info("hello"));
-		s.schedule(Actions.info("world"));
-		s.schedule(Actions.info("from immediate"));
-	}
+    @Test
+    public void testImmediate() {
+        Scheduler s = Schedulers.immediate();
+        s.schedule(Actions.info("hello"));
+        s.schedule(Actions.info("world"));
+        s.schedule(Actions.info("from immediate"));
+    }
 
-	@Test
-	public void testComputation() {
-		Scheduler s = Schedulers.computation();
-		s.schedule(Actions.info("hello"));
-		s.schedule(Actions.info("world"));
-		s.schedule(Actions.info("from computation"));
-	}
+    @Test
+    public void testComputation() {
+        Scheduler s = Schedulers.computation();
+        s.schedule(Actions.info("hello"));
+        s.schedule(Actions.info("world"));
+        s.schedule(Actions.info("from computation"));
+    }
 
-	@Test
-	public void testMergeSychronous() {
-		Streams.merge(Arrays.asList(Streams.from(Arrays.asList(1, 2, 3)),
-				Streams.from(Arrays.asList(4, 5, 6))));
-	}
+    @Test
+    public void testMergeSychronous() {
+        Streams.merge(Arrays.asList(Streams.from(Arrays.asList(1, 2, 3)),
+                Streams.from(Arrays.asList(4, 5, 6))));
+    }
+
+    @Test
+    public void testList() {
+        assertEquals(Arrays.asList(1, 2), Streams.just(1, 2).toList().single());
+    }
 
 }
