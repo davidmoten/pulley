d4d168de7829ba4d8145f736eeb1355be82fb2ee
Dave Moten <davidmoten@gmail.com>
2015-02-13 15:57:48 +1100

    check if found early as possible

--- src/main/java/pulley/streams/Merge.java
+++ src/main/java/pulley/streams/Merge.java
@@ -66,25 +66,111 @@ public class Merge {
             for (int i = 0; i < promises.size(); i++) {
                 final int index = i;
                 Promise<Optional<Cons<T>>> p = promises.get(index);
-                p.scheduler().schedule(new A0() {
-                    @Override
-                    public void call() {
-                        if (!found.get()) {
-                            promises2.set(index, Promises.cache(promises2.get(index)));
-                            Optional<Cons<T>> t = promises2.get(index).get();
-                            if (t.isPresent() && found.compareAndSet(false, true)) {
-                                value.set(t);
-                                promises2.set(index, t.get().tail());
-                                latch.countDown();
-                            } else if (!t.isPresent()) {
-                                completed.add(index);
-                                if (completed.size() == promises.size()) {
+                if (!found.get())
+                    p.scheduler().schedule(new A0() {
+                        @Override
+                        public void call() {
+                            if (!found.get()) {
+                                promises2.set(index, Promises.cache(promises2.get(index)));
+                                Optional<Cons<T>> t = promises2.get(index).get();
+                                if (t.isPresent() && found.compareAndSet(false, true)) {
+                                    value.set(t);
+                                    promises2.set(index, t.get().tail());
                                     latch.countDown();
+                                } else if (!t.isPresent()) {
+                                    completed.add(index);
+                                    if (completed.size() == promises.size()) {
+                                        latch.countDown();
+                                    }
                                 }
                             }
                         }
-                    }
-                });
+                    });
+            }
+            try {
+                latch.await();
+                if (completed.size() == promises.size())
+                    return Optional.absent();
+                else {
+                    return Optional.of(Cons.cons(value.get().get().head(), new MergePromise<T>(
+                            copyExcluding(promises2, completed))));
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private static <T> List<T> copyExcluding(List<T> list, Set<Integer> excludeIndexes) {
+            List<T> list2 = new ArrayList<T>();
+            for (int i = 0; i < list.size(); i++) {
+                if (!excludeIndexes.contains(i))
+                    list2.add(list.get(i));
+            }
+            return list2;
+        }
+
+        @Override
+        public A0 closeAction() {
+            return new A0() {
+                @Override
+                public void call() {
+                    for (Promise<Optional<Cons<T>>> promise : promises)
+                        promise.closeAction().call();
+                }
+            };
+        }
+
+        @Override
+        public Scheduler scheduler() {
+            return Schedulers.immediate();
+        }
+
+    }
+
+    private static class MergePromise2<T> implements Promise<Optional<Cons<T>>> {
+
+        private final Promise<Optional<Cons<Stream<T>>>> streamPromise;
+        private final List<Promise<Optional<Cons<T>>>> promises;
+
+        MergePromise2(Promise<Optional<Cons<Stream<T>>>> streamPromise,
+                List<Promise<Optional<Cons<T>>>> promises) {
+            this.streamPromise = streamPromise;
+            this.promises = promises;
+        }
+
+        @Override
+        public Optional<Cons<T>> get() {
+            // blocking !!!!
+            final List<Promise<Optional<Cons<T>>>> promises2 = new ArrayList<Promise<Optional<Cons<T>>>>(
+                    promises);
+            final AtomicBoolean found = new AtomicBoolean(false);
+            final AtomicReference<Optional<Cons<T>>> value = new AtomicReference<Optional<Cons<T>>>();
+            final CountDownLatch latch = new CountDownLatch(1);
+            final ConcurrentSkipListSet<Integer> completed = new ConcurrentSkipListSet<Integer>();
+
+            for (int i = 0; i < promises.size(); i++) {
+                final int index = i;
+                Promise<Optional<Cons<T>>> p = promises.get(index);
+                if (!found.get())
+                    p.scheduler().schedule(new A0() {
+                        @Override
+                        public void call() {
+                            if (!found.get()) {
+                                promises2.set(index, Promises.cache(promises2.get(index)));
+                                Optional<Cons<T>> t = promises2.get(index).get();
+                                if (t.isPresent() && found.compareAndSet(false, true)) {
+                                    value.set(t);
+                                    promises2.set(index, t.get().tail());
+                                    latch.countDown();
+                                } else if (!t.isPresent()) {
+                                    completed.add(index);
+                                    if (completed.size() == promises.size()) {
+                                        latch.countDown();
+                                    }
+                                }
+                            }
+                        }
+                    });
             }
             try {
                 latch.await();
