014e9c330223d056f0a8c48a7eda5c6b38327c5e
Dave Moten <davidmoten@gmail.com>
2015-02-16 12:48:05 +1100

    start Result refactor

--- src/main/java/pulley/Result.java
+++ src/main/java/pulley/Result.java
@@ -1,4 +1,44 @@
 package pulley;
 
-public interface Result<T> {
+import pulley.util.Optional;
+import pulley.util.Preconditions;
+
+public class Result<T> {
+
+    private final Optional<T> value;
+    private final Optional<Throwable> throwable;
+
+    public Result(Optional<T> value, Optional<Throwable> throwable) {
+        Preconditions.checkArgument(!value.isPresent() || !throwable.isPresent(),
+                "both can't be present");
+        this.value = value;
+        this.throwable = throwable;
+    }
+
+    public static <T> Result<T> of(T t) {
+        return new Result<T>(Optional.of(t), Optional.<Throwable> absent());
+    }
+
+    public static <T> Result<T> error(Throwable e) {
+        return new Result<T>(Optional.<T> absent(), Optional.of(e));
+    }
+
+    public Optional<T> value() {
+        if (throwable.isPresent())
+            return Exceptions.throwException(throwable.get());
+        else
+            return value;
+    }
+
+    public T or(T t) {
+        if (throwable.isPresent())
+            return Exceptions.throwException(throwable.get());
+        else
+            return value.or(t);
+    }
+
+    public Optional<Throwable> error() {
+        return throwable;
+    }
+
 }
