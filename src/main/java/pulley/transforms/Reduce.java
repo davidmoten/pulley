ec884d1c161a8c3517587db3d08f338d7b4cc032
Dave Moten <davidmoten@gmail.com>
2015-02-19 14:28:22 +1100

    white space

--- src/main/java/pulley/transforms/Reduce.java
+++ src/main/java/pulley/transforms/Reduce.java
@@ -13,40 +13,42 @@ import pulley.util.Optional;
 
 public class Reduce {
 
-    public static <T, R> Stream<R> reduce(Stream<T> stream, R initial,
-            F2<? super R, ? super T, ? extends R> reducer) {
-        return stream.transform(new ReduceTransformer<T, R>(initial, reducer));
-    }
+	public static <T, R> Stream<R> reduce(Stream<T> stream, R initial,
+			F2<? super R, ? super T, ? extends R> reducer) {
+		return stream.transform(new ReduceTransformer<T, R>(initial, reducer));
+	}
 
-    private static class ReduceTransformer<T, R> implements Transformer<T, R> {
+	private static class ReduceTransformer<T, R> implements Transformer<T, R> {
 
-        private final R initial;
-        private final F2<? super R, ? super T, ? extends R> reducer;
+		private final R initial;
+		private final F2<? super R, ? super T, ? extends R> reducer;
 
-        public ReduceTransformer(R initial, F2<? super R, ? super T, ? extends R> reducer) {
-            this.initial = initial;
-            this.reducer = reducer;
-        }
+		public ReduceTransformer(R initial,
+				F2<? super R, ? super T, ? extends R> reducer) {
+			this.initial = initial;
+			this.reducer = reducer;
+		}
 
-        @Override
-        public Promise<Optional<Cons<R>>> transform(final Promise<Optional<Cons<T>>> promise) {
-            return new AbstractStreamPromise<T, R>(promise) {
+		@Override
+		public Promise<Optional<Cons<R>>> transform(
+				final Promise<Optional<Cons<T>>> promise) {
+			return new AbstractStreamPromise<T, R>(promise) {
 
-                @Override
-                public Optional<Cons<R>> get() {
-                    final AtomicReference<R> ref = new AtomicReference<R>(initial);
-                    A1<T> action = new A1<T>() {
+				@Override
+				public Optional<Cons<R>> get() {
+					final AtomicReference<R> ref = new AtomicReference<R>(
+							initial);
+					A1<T> action = new A1<T>() {
+						@Override
+						public void call(T t) {
+							ref.set(reducer.call(ref.get(), t));
+						}
+					};
+					Stream.forEach(promise, action);
+					return Optional.of(Cons.cons(ref.get()));
+				}
 
-                        @Override
-                        public void call(T t) {
-                            ref.set(reducer.call(ref.get(), t));
-                        }
-                    };
-                    Stream.forEach(promise, action);
-                    return Optional.of(Cons.cons(ref.get()));
-                }
-
-            };
-        }
-    }
+			};
+		}
+	}
 }
